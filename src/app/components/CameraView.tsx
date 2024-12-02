7db3c83f7477123e381d92c978c1a6a62d1f15cf
CarlosPisco <edupiscote.cept@gmail.com>
2024-12-02 00:01:02 -0500

    paris n'est pluc avec nous

--- src/app/components/CameraView.tsx
+++ src/app/components/CameraView.tsx
@@ -6,14 +6,14 @@ export default function CameraView() {
         {/* Video en vivo incrustado con iframe */}
         
         <iframe
-          className="w-full h-full"
-          src="https://www.youtube.com/embed/cdTRCcj4DkU?autoplay=1&mute=1&si=S5A6J7VzpC6S0YOr"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen
-        ></iframe>
+        className="w-full h-full"
+        src="https://www.youtube.com/embed/lsoaKuDWTOc?autoplay=1&mute=1&si=Teql7OzOiNJcLLbS"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
 
         {/* Anotaciones dinámicas 
         <div className="absolute bottom-4 left-4 bg-red-600 text-white p-2 rounded-md">
